c2ea7d096ee42028552641e8d398d885600d9d46
thhuyen <97439051+thhuyen@users.noreply.github.com>
2022-11-25 18:00:19 +0700

    set giá và tổng cộng

--- assets/PHP/test.php
+++ assets/PHP/test.php
@@ -11,12 +11,13 @@
         'price' => $get_data_roomtype["RoomTypePrice"],
         ]);
     }
+    //print_r($arr);
     $output = array_map(function ($object) {
          return $object->name . ': '. $object->price; 
         }, $arr);
-    echo implode(', ', $output);
-   
-    // echo http_build_query($arr,'',', ');
+    // echo implode(', ', $output); // dòng này là conver array to string thui chứ k gì
+    // echo "\n";
+    // echo (gettype($arr[0])); // yecasi output là tui đổi v để cho về string nhưng mà quên :v hehe
    
 
 ?>
